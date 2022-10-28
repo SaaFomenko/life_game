c953675dda465e26475a692188804f4268ba7227
SaaFomenko <s.fomenko@list.ru>
2022-10-28 11:22:58 +0300

    Optimised code.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <fstream>
 
-/*
-int* getTerms(std::string path)
+void viewArr(bool** arr, int* rows, int* cols)
 {
+  std::cout << std::endl;
 
+  for (int i = 0; i < *rows; ++i)
+	{
+		for (int j = 0; j < *cols; ++j)
+		{
+			char s = arr[i][j] ? '*' : '-';
+
+			std::cout << s << " ";
+		}
+
+		std::cout << std::endl;
+	}
+  
+  std::cout << std::endl;
 }
-*/
 
 int main() {
   const std::string msg[] = {
     "Не удалось прочитать файл, проерьте его наличие и праава доступа: ",
   };
-/*
-  const int arr[][6] = {
-    0, -24, 4, 10, 11, 22,
-    33, 32, 44, 56, 67, 78,
-    89, -12, 25, 99, 100, 128,
-  };
-  const int rows = sizeof(arr) / sizeof(arr[0]);
-  const int colls = sizeof(arr[0]) / sizeof(arr[0][0]);
-  const int first_index = 0;
-  const int last_index = colls - 1;
-  std::cout << max_element_index[1] << std::endl;
-*/  
-
 	const char* path = { "./in.txt" };
 
 	std::ifstream fin (path);
@@ -38,8 +37,6 @@ int main() {
 
 	int rows_size = 0;
 	int cols_size = 0;
-	int val = 0;
-	int stat[] = {0, 0};
 
 	fin >> rows_size;
 	fin >> cols_size;
@@ -52,34 +49,18 @@ int main() {
 //		std::cout << arr_back[i][i] << std::endl;
 	}
 
-	int i = 0;
-	while (fin >> val)
-	{
-		if (i%2)
-		{
-			stat[1] = val;
-			arr_back[stat[0]][stat[1]] = true;
-		}
-		else
-		{
-			stat[0] = val;
-		}
-
-		++i;
-	}
-
-	for (int i = 0; i < rows_size; ++i)
-	{
-		for (int j = 0; j < cols_size; ++j)
-		{
-			char s = arr_back[i][j] ? '*' : '-';
-
-			std::cout << s << " ";
-		}
-
-		std::cout << std::endl;
-	}
-
+  
+  for (
+    int i = 0,
+    j = 0;
+    (!fin.eof());
+  ) {
+    fin >> i;
+    fin >> j;
+    arr_back[i][j] = true;
+  }
+  
+	viewArr(arr_back, &rows_size, &cols_size);
 
   return 0;
 }
